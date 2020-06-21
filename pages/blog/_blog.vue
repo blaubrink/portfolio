af5c96f67264800d697216db4c2ea39b5e0df95b
blaubrink <lauritzbrinkmann@gmail.com>
2020-06-21 19:51:59 +0200

    configure netlify cms

---
+++ pages/blog/_blog.vue
@@ -0,0 +1,24 @@
+<template>
+  <article style="text-align: center">
+    <h1>{{ blogPost.title }}</h1>
+    <p>Published at: {{ blogPost.date }}</p>
+    <p style="font-style: italic">{{ blogPost.description }}</p>
+    <div>{{ blogPost.body }}</div>
+  </article>
+</template>
+
+<script>
+export default {
+  async asyncData({ params, payload }) {
+    if (payload) return { blogPost: payload };
+    else
+      return {
+        blogPost: await require(`~/assets/content/blog/${params.blog}.json`),
+      };
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/blog/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ blogPosts }}</div>
+</template>
+
+<script>
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
